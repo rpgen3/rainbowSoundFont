cde40e02bac8ee0d9e2c594f3248b70ad6cd72ca
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2023-01-17 22:04:25 +0900

    Update and rename parser.js to mjs/fetchRainbowSoundFont.mjs

---
+++ mjs/fetchRainbowSoundFont.mjs
@@ -0,0 +1,68 @@
+
+class Node {
+    constructor() {
+        this.children = [];
+    }
+}
+
+const parseAttributes = str => str.match(/[^ ]+=".+?"/g)?.map(v => v.split('=')).map(([a, b]) => [a, b.slice(1, -1)]);
+const endOf = array => array[array.length - 1];
+
+const parseListXML = listXML => {
+    const stack = [new Node];
+    listXML.split('\n').map(v => v.match(/<.+?>/)[0]).forEach(v => {
+        const attributes = Object.fromEntries(parseAttributes(v) || []);
+        const isClosed = v.slice(-2) === '/>';
+        const tag = v.replace(/ .*$/g, '');
+        switch (tag) {
+            case '<Map':
+                const node = Object.assign(new Node, attributes);
+                endOf(stack).children.push(node);
+                if (!isClosed) {
+                    stack.push(node);
+                }
+                break;
+            case '<PC':
+                endOf(stack).children.push(attributes);
+                break;
+            case '<Bank':
+                Object.assign(endOf(endOf(stack).children), attributes);
+                break;
+            case '</PC>':
+                break;
+            case '</Map>':
+                stack.pop();
+                break;
+        }
+    });
+    return stack[0];
+};
+
+export const fetchRainbowSoundFont = async () => {
+    const xml = await (await fetch('https://rpgen3.github.io/rainbowSoundFont/xml/RaInBow%20SoundFont%202017.xml')).text();
+
+    const instrumentListXML = xml.match(/<InstrumentList>(.|\n)+?<\/InstrumentList>/)[0];
+    const drumSetListXML = xml.match(/<DrumSetList>(.|\n)+?<\/DrumSetList>/)[0];
+
+    const instrumentList = parseListXML(instrumentListXML);
+    const drumSetList = parseListXML(drumSetListXML);
+
+    const group = [...instrumentList.children].filter(v => v.children.length === 0);
+    const groupSet = new Set(group);
+
+    let index = -1;
+    for (const v of instrumentList.children) {
+        if (groupSet.has(v)) {
+            index++;
+        } else {
+            group[index].children.push(v);
+        }
+    }
+
+    group[0].children.shift();
+
+    return {
+        instrumentMap: group,
+        drumSetMap: drumSetList.children[0].children,
+    };
+};
